5afbc13246344fff75b710f7446a5cad8d045eac
Pepijn Blom <pepijn@graciousstudios.nl>
2016-07-06 13:53:02 +0200

    cleanup

--- app/code/local/GraciousStudios/Margins/sql/margins_setup/mysql4-install-0.1.0.php
+++ app/code/local/GraciousStudios/Margins/sql/margins_setup/mysql4-install-0.1.0.php
@@ -35,30 +35,4 @@ $installer->addAttribute('catalog_product', 'cost', [
     'unique'           => false,
 ]);
 
-//$installer->run("DROP TABLE IF EXISTS {$installer->getTable('gs_margins')};");
-
-$installer->run("
-    CREATE TABLE {$installer->getTable('gs_margins')} (
-        `entity_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-        `product_id` INT(10) UNSIGNED NOT NULL,
-        `sku` VARCHAR(255) NOT NULL,
-        `name` TEXT NOT NULL,
-        `total_qty` INT(11) NOT NULL DEFAULT 0,
-        `purchase_price` FLOAT NOT NULL,
-        `revenue_excl_tax` FLOAT NOT NULL,
-        `revenue_incl_tax` FLOAT NOT NULL,
-        `avg_sale_price` FLOAT NOT NULL,
-        `tax_amount` FLOAT NOT NULL,
-        `percentage_excl_tax` FLOAT NOT NULL,
-        `percentage_incl_tax` FLOAT NOT NULL,
-        `margin_incl_tax` FLOAT NOT NULL,
-        `margin_excl_tax` FLOAT NOT NULL,
-        `created_at` DATETIME NULL,
-        `updated_at` DATETIME NULL,
-        PRIMARY KEY (`entity_id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-    ALTER TABLE {$installer->getTable('gs_margins')} ADD INDEX `gs_margins_product_id` (`product_id`);
-");
-
-$installer->setConfigData('gracefuldeals/configuration/enabled', 1);
 $installer->endSetup();
